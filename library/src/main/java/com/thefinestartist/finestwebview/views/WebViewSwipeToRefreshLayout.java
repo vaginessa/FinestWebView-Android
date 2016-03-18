ece2fc6a1da704f525a4d8979fece302c5c9ccaf
Leonardo Taehwan Kim <leonardo@thefinestartist.com>
2016-03-19 00:12:55 +0900

    Update widget spliting

--- library/src/main/java/com/thefinestartist/finestwebview/views/WebViewSwipeToRefreshLayout.java
+++ library/src/main/java/com/thefinestartist/finestwebview/views/WebViewSwipeToRefreshLayout.java
@@ -14,17 +14,17 @@ import com.thefinestartist.converters.UnitConverter;
 /**
  * Created by TheFinestArtist on 3/12/16.
  */
-public class CustomSwipeToRefresh extends SwipeRefreshLayout {
+public class WebViewSwipeToRefreshLayout extends SwipeRefreshLayout {
 
     private static final int SCROLL_BUFFER_DIMEN = 1;
     private static int scrollBuffer;
     private WebView webView;
 
-    public CustomSwipeToRefresh(Context context) {
+    public WebViewSwipeToRefreshLayout(Context context) {
         super(context);
     }
 
-    public CustomSwipeToRefresh(Context context, AttributeSet attrs) {
+    public WebViewSwipeToRefreshLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
